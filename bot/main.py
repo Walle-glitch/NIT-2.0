9b64bbbb6f53d4f0ae4c5beb82e8f6172440fb6e
Walle-glitch <102384388+Walle-glitch@users.noreply.github.com>
2024-08-17 15:04:37 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -34,9 +34,7 @@ async def on_message(message):
         await message.channel.send(Reply)
         return
     if message.content.startswith('./hello'):
-        Reply = 'Hello! '/n 
-        'I am the NIT-Bot and exists on GitHub, Feel free to Commit to me ;) '/n 
-        'Curently only ./git , ./who and ./hello exits... Feal free to add more !' 
+        Reply = 'Hello! I am the NIT-Bot and exists on GitHub, Feel free to Commit to me ;) Curently only ./git , ./who and ./hello exits... Feal free to add more !' 
         await message.channel.send(Reply)
         return    
     return
