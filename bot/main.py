362cdaf66d23ea92b2fefcc4a0ca3c126e9ec519
Walle-glitch <102384388+Walle-glitch@users.noreply.github.com>
2024-09-02 22:42:23 +0200

    Fixed The "New Member issue"

--- bot/main.py
+++ bot/main.py
@@ -21,7 +21,7 @@ import _CCNP_Study_Plan  # CCNP study plan module.
 
 ###########################################_Global_Variables_##########################################
 
-version_nr = "Current Version is 24/09/02.1"  # Global version number variable
+version_nr = "Current Version is 24/09/02.2"  # Global version number variable
 
 # Roles with access to "Sudo commands"
 BOT_ADMIN_ROLE_NAME = "Bot-Master"
@@ -297,7 +297,7 @@ async def update_roles():
         await log_to_channel(bot, f"An error occurred: {str(e)}")
 
 @bot.command()
-async def roll(ctx, *, role_name: str):
+async def role(ctx, *, role_name: str):
     try:    
         await _Bot_Modul.assign_role(ctx, role_name)
     except Exception as e:
@@ -329,60 +329,70 @@ async def rfc(ctx, rfc_number: str = None):
 
 ###########################################_Below this line_###########################################
 
-# Role name to be assigned by the bot
-ROLE_NAME = "YourRoleName"
+def load_roles():
+    try:
+        with open(ROLE_JSON_FILE, 'r') as file:
+            roles = json.load(file)
+        return roles
+    except Exception as e:
+        print(f"Failed to load roles: {str(e)}")
+        return {}
 
 @bot.command()
-async def addrole(ctx):
+async def addrole(ctx, role_name: str = None):
     """
-    Assigns a specific role to the user running the command.
-    Uses an embedded message to provide feedback.
+    Assigns a specific role to the user running the command. Lists available roles if none specified or role not found.
     """
-    try:
-        role = discord.utils.get(ctx.guild.roles, name=ROLE_NAME)
-        
-        if role is None:
-            await ctx.send(f"Role '{ROLE_NAME}' could not be found on the server.")
-            return
+    roles = load_roles()
+    
+    if role_name is None or role_name not in roles:
+        role_list = ', '.join(roles.keys())
+        await ctx.send(f"Role '{role_name}' could not be found. Available roles: {role_list}")
+        return
 
-        if role in ctx.author.roles:
-            embed = discord.Embed(title="Role Assigned", description=f"You already have the role **{ROLE_NAME}**.", color=discord.Color.orange())
-        else:
-            try:
-                await ctx.author.add_roles(role)
-                embed = discord.Embed(title="Role Assigned", description=f"The role **{ROLE_NAME}** has been assigned to you!", color=discord.Color.green())
-            except discord.Forbidden:
-                embed = discord.Embed(title="Error", description="I do not have sufficient permissions to assign this role.", color=discord.Color.red())
-        
-        await ctx.send(embed=embed)
-    except Exception as e:
-        await ctx.send(f"An error occurred: {str(e)}")
+    role = discord.utils.get(ctx.guild.roles, name=role_name)
+    if role is None:
+        await ctx.send(f"Role '{role_name}' could not be found on this server.")
+        return
+
+    if role in ctx.author.roles:
+        embed = discord.Embed(title="Role Already Assigned", description=f"You already have the role **{role_name}**.", color=discord.Color.orange())
+    else:
+        try:
+            await ctx.author.add_roles(role)
+            embed = discord.Embed(title="Role Assigned", description=f"The role **{role_name}** has been assigned to you!", color=discord.Color.green())
+        except discord.Forbidden:
+            embed = discord.Embed(title="Error", description="I do not have sufficient permissions to assign this role.", color=discord.Color.red())
+    
+    await ctx.send(embed=embed)
 
 @bot.command()
-async def removerole(ctx):
+async def removerole(ctx, role_name: str = None):
     """
-    Removes a specific role from the user running the command.
-    Uses an embedded message to provide feedback.
+    Removes a specific role from the user running the command. Lists available roles if none specified or role not found.
     """
-    try:
-        role = discord.utils.get(ctx.guild.roles, name=ROLE_NAME)
-        
-        if role is None:
-            await ctx.send(f"Role '{ROLE_NAME}' could not be found on the server.")
-            return
+    roles = load_roles()
+    
+    if role_name is None or role_name not in roles:
+        role_list = ', '.join(roles.keys())
+        await ctx.send(f"Role '{role_name}' could not be found. Available roles: {role_list}")
+        return
 
-        if role not in ctx.author.roles:
-            embed = discord.Embed(title="Role Removed", description=f"You do not have the role **{ROLE_NAME}**.", color=discord.Color.orange())
-        else:
-            try:
-                await ctx.author.remove_roles(role)
-                embed = discord.Embed(title="Role Removed", description=f"The role **{ROLE_NAME}** has been removed from you.", color=discord.Color.green())
-            except discord.Forbidden:
-                embed = discord.Embed(title="Error", description="I do not have sufficient permissions to remove this role.", color=discord.Color.red())
-        
-        await ctx.send(embed=embed)
-    except Exception as e:
-        await ctx.send(f"An error occurred: {str(e)}")
+    role = discord.utils.get(ctx.guild.roles, name=role_name)
+    if role is None:
+        await ctx.send(f"Role '{role_name}' could not be found on this server.")
+        return
+
+    if role not in ctx.author.roles:
+        embed = discord.Embed(title="Role Not Found", description=f"You do not have the role **{role_name}**.", color=discord.Color.orange())
+    else:
+        try:
+            await ctx.author.remove_roles(role)
+            embed = discord.Embed(title="Role Removed", description=f"The role **{role_name}** has been removed from you.", color=discord.Color.green())
+        except discord.Forbidden:
+            embed = discord.Embed(title="Error", description="I do not have sufficient permissions to remove this role.", color=discord.Color.red())
+    
+    await ctx.send(embed=embed)
 
 ###########################################_Study_Plan_Loops_###########################################
 
@@ -418,12 +428,31 @@ async def job_posting_loop():
 # Welcome Message
 @bot.event
 async def on_member_join(member):
+    """
+    Sends a greeting message in the server's designated welcome channel and a private message to new members.
+    Also logs errors to a specific channel if unable to send a DM.
+    """
+    # Fetch the welcome channel using the global variable
+    welcome_channel = bot.get_channel(WELCOME_CHANNEL_ID)
+    if welcome_channel:
+        await welcome_channel.send(f"Welcome to the server, {member.mention}! 🎉 Please check your DM for more info!")
+
+    # Attempt to send a private message to the new member
     try:
-        channel = discord.utils.get(member.guild.text_channels, name="welcome")
-        if channel:
-            await channel.send(f"Welcome to the server, {member.mention}!")
-    except Exception as e:
-        print(f"An error occurred while welcoming a new member: {str(e)}")
+        await member.send(
+            "Hello and welcome! 🎉\n\n"
+            "Here's some information to get you started:\n"
+            "- Make sure to read the rules and verify yourself Your start Year in the FYI chanel\n"
+            "- You can introduce yourself in the Skit-snack channel.\n"
+            "- If you have any questions, feel free to ask a moderator(Privilege 10) or admin (Privilege 15).\n\n"
+            "Enjoy your time here!"
+        )
+
+    except discord.Forbidden:
+        # Log error to the log channel
+        log_channel = bot.get_channel(LOG_CHANNEL_ID)
+        if log_channel:
+            await log_channel.send(f"Could not send DM to {member.mention}. They might have DMs disabled.")
 
 # XP Levels Handling
 @bot.event
@@ -537,7 +566,6 @@ async def test(ctx):
         ("stop_game", None),
         ("start_game", "network"),
         ("stop_game", None),
-        ("Reboot", None),
     ]
     
     for command_name, arg in test_commands:
