3287445274f07023d3ea679accc46f6665ab2628
Walle-glitch <102384388+Walle-glitch@users.noreply.github.com>
2024-09-11 22:39:35 +0200

    .

---
+++ bot/_Bot_Config_Default.py
@@ -0,0 +1,29 @@
+'''
+Replace 'HERE' with your respective Tokens and Keys 
+Rename the file to _Bot_Config.py
+
+mv _Bot_Config_Default.py _Bot_Config.py
+
+'''
+
+# Bot Token 
+def _Bot_Token():
+    return 'HERE'
+# Bot Client ID
+def _Client_ID():
+    return 'HERE'
+
+# Bot Seceret
+def _Client_Secret():
+    return 'HERE'
+
+# API Key to OpenAI
+def _Open_AI_Token():
+    return 'HERE'
+
+# API key to Indeed
+def _YOUR_INDEED_API_KEY():
+    return 'HERE'
+
+
+
---
+++ bot/_Router_Conf_Default.py
@@ -0,0 +1,11 @@
+'''
+Used for the Funktion BGP-Setup
+Rename the file _Router_Conf.py even if you wont use this funktion. 
+Or delete the funktion in the code 
+
+mv _Router_Conf_Default.py _Router_Conf.py
+
+'''
+ROUTER_IP = "IP-Address" # Router IP-Address
+SSH_USERNAME = "Username"  # Ditt SSH-användarnamn
+SSH_PASSWORD = "Password"  # Ditt SSH-lösenord
